93b343b0632aa69b306ce013a77c80b9065a5814
bolandt2 <101474767+bolandt2@users.noreply.github.com>
2022-05-01 07:55:50 -0400

    Add files via upload

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,53 @@
+-- List of employees titles that are retiring
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- List of employees unique titles that are retiring
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title
+INTO unique_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, t.to_date DESC;
+
+
+-- The number of unique titles for employees by their most recent job title who are about to retire
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+
+-- List of employees titles that are retiring
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_employee AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+    AND (de.to_date = '9999-01-01')
+	AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no;
